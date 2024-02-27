c464fb81fb179add26d8a42aad6038522703f189
Andrew Roberts <andrew@azroberts.com>
2024-02-27 11:46:04 -0500

    Moved GPT API requests to separate module to increase code readability

--- main.ts
+++ main.ts
@@ -1,50 +1,11 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import { GPTQuery } from "./core.ts";
 
-const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
 interface APIResponse {
   status: string;
   input: string;
   output?: string;
-}
-
-enum SystemPrompt {
-  BulletPoints
-}
-
-const systemPrompts = new Map<SystemPrompt, string>([
-  [SystemPrompt.BulletPoints, `You are the elite resume writer. For each prompt users will provide a short description of a prior work experience they have had. As the elite resume writer, your job is to summarize the job description into 5 resume bullet points. These bullet points should sound professional and showcase leadership, technical proficiency, and achievements through the use of strong action statements and adjectives and quantifiable metrics. Bullet points should favor concision and should not exceed 18 words. Each bullet point should start with a "•" character. Do not make up details or metrics that were not stated in the description provided by the user.`]
-]);
-
-async function askGPT(prompt: SystemPrompt, input: string): Promise<string> {
-  const gptResponse = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: systemPrompts.get(prompt)
-        },
-        {
-          role: "user",
-          content: input
-        }
-      ],
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0.3,
-      presence_penalty: 0
-    })
-  })
-    .then(response => response.json())
-
-  return gptResponse.choices[0].message.content;
 }
 
 const router = new Router();
@@ -58,7 +19,10 @@ router
     };
 
     if(typeof description === 'string') {
-      const GPTResponse = await askGPT(SystemPrompt.BulletPoints, description);
+      const GPTResponse = await GPTQuery({
+        systemPrompt: `You are the elite resume writer. For each prompt users will provide a short description of a prior work experience they have had. As the elite resume writer, your job is to summarize the job description into 5 resume bullet points. These bullet points should sound professional and showcase leadership, technical proficiency, and achievements through the use of strong action statements and adjectives and quantifiable metrics. Bullet points should favor concision and should not exceed 18 words. Each bullet point should start with a "•" character. Do not make up details or metrics that were not stated in the description provided by the user.`,
+        userInput: description
+      });
       response.status = "success";
       response.output = GPTResponse;
     } else {
